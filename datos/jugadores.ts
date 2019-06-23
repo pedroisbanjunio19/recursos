5956602cf3610fd6ba2a40e76337a6297df1f0f8
pedroisbanjunio19 <pedroisbanjunio19.gmail.com>
2019-06-23 10:30:05 +0200

    sunday

---
+++ datos/jugadores.ts
@@ -0,0 +1,14 @@
+locales: any[] = [
+  {nombre: 'Isaiah Thomas', puntos: 0, imagen: 'assets/img/thomas.png'},
+  {nombre: 'Kyle Kuzma', puntos: 0, imagen: 'assets/img/kuzma.png'},
+  {nombre: 'Julius Randle', puntos: 0, imagen: 'assets/img/randle.png'},
+  {nombre: 'Brandom Ingram', puntos: 0, imagen: 'assets/img/ingram.png'},
+  {nombre: 'Brook López', puntos: 0, imagen: 'assets/img/lopez.png'},
+];
+visitantes: any[] = [
+  {nombre: 'Kaadem Allen', puntos: 0, imagen: 'assets/img/allen.png'},
+  {nombre: 'Aron Bynes', puntos: 0, imagen: 'assets/img/bynes.png'},
+  {nombre: 'Jabari Bird', puntos: 0, imagen: 'assets/img/bird.png'},
+  {nombre: 'Jaylen Brown', puntos: 0, imagen: 'assets/img/brown.png'},
+  {nombre: 'Jonathan Gibson', puntos: 0, imagen: 'assets/img/gibson.png'},
+];
